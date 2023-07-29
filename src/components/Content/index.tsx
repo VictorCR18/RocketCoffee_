417acfe3a11132d1bc44d2e3b0790ec66f253de9
VictorCR18 <cavalcantevictor2003@gmail.com>
2023-07-29 12:52:03 -0300

    finalizado

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,17 +1,22 @@
-import Copos from '../../assets/rocket-coffee.png'
-
+import Copos from "../../assets/rocket-coffee.png";
+import Flecha from "../../assets/arrow.svg";
 import "./styles.scss";
 
 export function Content() {
   return (
     <div className="content-content">
+      <div className="textoM">
+        <label>O café que fará seu código decolar para o próximo nível.</label>
+      </div>
+      <div className="button-header2">
+        <a href="/">PEGAR MEU CAFÉ </a>
+        <img src={Flecha} />
+      </div>
       <div className="titulo-content">
         <h1>Great Coffee</h1>
         <h2>&lt;Great Code/&gt;</h2>
       </div>
-      <div className="copos-content">
-        <img src={Copos}/>
-      </div>
+      <div className="copos-content"><img src={Copos}/></div>
     </div>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,66 @@
-import "./styles.scss";
+import * as Dialog from "@radix-ui/react-dialog";
+
+import Close from "../../assets/menu-buguer-close.svg"
 import LogoD from "../../assets/logo-desktop.svg";
+import LogoM from "../../assets/logo-mobile.svg";
+
+import "./styles.scss";
 
 export function Header() {
+  const Modal = () => (
+    <Dialog.Root>
+      <Dialog.Trigger asChild>
+        <label htmlFor="checkbox" className="hamburguer">
+          <span className="line line--top"></span>
+          <span className="line line--middle"></span>
+        </label>
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className="DialogOverlay" />
+        <Dialog.Content className="DialogContent">
+          <Dialog.Title className="DialogTitle">
+            {" "}
+            <div className="logoModal">
+              <img className="union" src={LogoM} />
+              <Dialog.Close asChild>
+                <button className="IconButton" aria-label="Close">
+                  <img src={Close} />
+                </button>
+              </Dialog.Close>
+            </div>
+          </Dialog.Title>
+          <Dialog.Description className="DialogDescription">
+            <div className="navbar-modal">
+              <a href="/" className="label-header">
+                Home
+              </a>
+              <a href="/" className="label-header">
+                Menu
+              </a>
+              <a href="/" className="label-header">
+                Recompensas
+              </a>
+              <a href="/" className="label-header">
+                Gift Cards
+              </a>
+              <a href="/" className="label-header">
+                Lojas
+              </a>
+            </div>
+          </Dialog.Description>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+
   return (
     <div className="content-header">
+      <div className="navM">
+        <div className="logo">
+          <img className="union" src={LogoM} />
+        </div>
+        <Modal />
+      </div>
       <nav className="nav">
         <div>
           <img className="logoD" src={LogoD} />
