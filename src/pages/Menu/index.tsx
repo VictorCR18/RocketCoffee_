abb883e1a9f1fd10e986abaf3903ea1f73ac8b08
VictorCR18 <cavalcantevictor2003@gmail.com>
2023-07-31 14:00:37 -0300

    adc modal do card

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,13 +1,44 @@
+import * as Dialog from "@radix-ui/react-dialog";
+
 import { Card } from "../../components/Card";
 
 import "./styles.scss";
+import { Cross2Icon } from "@radix-ui/react-icons";
 
-export function Menu(){
-    return(
-        <div className="content-menu">
-            <Card />
-            <Card />
-            <Card />
-        </div>
-    )
+export function Menu() {
+  const DialogDemo = () => (
+    <Dialog.Root>
+      <Dialog.Trigger asChild>
+        <button className="ButtonCard violet">Edit profile</button>
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className="DialogOverlayCard" />
+        <Dialog.Content className="DialogContentCard">
+          <div className="imageProd"></div>
+          <div className="conteudo">
+            <Dialog.Title className="DialogTitleCard">
+              Edit profile
+            </Dialog.Title>
+            <Dialog.Description className="DialogDescriptionCard">
+              Make changes to your profile here. Click save when you're done.
+            </Dialog.Description>
+            <Dialog.Close asChild>
+              <button className="IconButtonCard" aria-label="Close">
+                <Cross2Icon />
+              </button>
+            </Dialog.Close>
+          </div>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+
+  return (
+    <div className="content-menu">
+      <Card />
+      <Card />
+      <Card />
+      <DialogDemo />
+    </div>
+  );
 }
